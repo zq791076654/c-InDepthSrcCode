add9b5dd5ea2ae97c6b32468c9fbed6778870b09
ZhuQiang <791076654@qq.com>
2019-07-29 20:58:23 +0800

    modify

---
+++ Chapter01/CSharp3/ListSortWithDelegate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter01.CSharp3
+{
+	class ListSortWithDelegate
+	{
+		static void Main()
+		{
+			List<Product> productList = Product.GetSampleProducts();
+			productList.Sort
+			(
+				delegate (Product x, Product y)
+				{
+					return x.Name.CompareTo(y.Name);
+				}
+			);
+
+			foreach (Product item in productList)
+			{
+				Console.WriteLine(item.Name);
+			}
+		}
+	}
+}
